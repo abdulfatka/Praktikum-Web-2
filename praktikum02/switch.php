b0a5ca5d2745e064f260657da37436de8190e455
abdulfatka <abdulfatka123@gmail.com>
2024-03-07 19:43:46 +0700

    Tugas Praktikum 02

---
+++ praktikum02/switch.php
@@ -0,0 +1,27 @@
+<?php
+$grade = "E";
+
+$predikat;
+
+switch ($grade) {
+    case "A":
+        $predikat = "Sangat Baik";
+        break;
+    case "B":
+        $predikat = "Baik";
+        break;
+    case "C":
+        $predikat = "Cukup";
+        break;
+    case "D":
+        $predikat = "Kurang";
+        break;
+    case "E":
+        $predikat = "Wassalam";
+        break;
+    default:
+        $predikat = "Nilai Tidak Ditemukan";
+        break;
+}
+
+echo "Predikat : " . $predikat;
