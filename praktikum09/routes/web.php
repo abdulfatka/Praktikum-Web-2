7f60799d56d1aabf615e74cfd112e5a24c9511ae
abdulfatka <abdulfatka123@gmail.com>
2024-05-31 06:01:49 +0700

    Praktikum 9, 10, 11

--- praktikum09/routes/web.php
+++ praktikum09/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DokterController;
+use App\Http\Controllers\PasienController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PeriksaController;
+use App\Http\Controllers\UnitKerjaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,8 +30,18 @@ Route::get('/kabar', function () {
 Route::get('/pasien', function () {
     return view('pasien');
 });
+Route::get('/admin/dokter', [DokterController::class, 'index']);
 
-Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/admin/unit_kerja', [UnitKerjaController::class, 'index']);
 
-Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::get('/admin/periksa', [PeriksaController::class, 'index']);
 
+Route::get('/admin',[AdminController::class,'index']);
+
+Route::get('/admin/pasien', [PasienController::class, 'index'])->name('pasiens.index');
+Route::get('/admin/pasien/create', [PasienController::class, 'create'])->name('pasiens.create');
+Route::post('/admin/pasien/store', [PasienController::class, 'store'])->name('pasiens.store');
+Route::get('/admin/pasien/{pasien}', [PasienController::class, 'show'])->name('pasiens.show');
+Route::get('/admin/pasien/{pasien}/edit', [PasienController::class, 'edit'])->name('pasiens.edit');
+Route::put('/admin/pasien/{pasien}', [PasienController::class, 'update'])->name('pasiens.update');
+Route::delete('/admin/pasien/{pasien}', [PasienController::class, 'destroy'])->name('pasiens.destroy');
